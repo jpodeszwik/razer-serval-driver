156ba16ac3a830d988fdafcd98b48c73651c011f
Jakub Podeszwik <jakubpodeszwik@gmail.com>
2016-10-11 23:51:25 +0200

    Using left analog stick as arrow keys

--- serval.py
+++ serval.py
@@ -19,7 +19,35 @@ dev = find_razer_serval()
 if dev != None:
     for event in dev.read_loop():
         if event.type == 3:
-            if event.code == 16:
+            if event.code == 0:
+                if event.value < 128:
+                    print('left')
+                    ui.write(e.EV_KEY, e.KEY_LEFT, 1)
+                    ui.syn()
+                elif event.value > 128:
+                    print('right')
+                    ui.write(e.EV_KEY, e.KEY_RIGHT, 1)
+                    ui.syn()
+                else:
+                    print('release')
+                    ui.write(e.EV_KEY, e.KEY_RIGHT, 0)
+                    ui.write(e.EV_KEY, e.KEY_LEFT, 0)
+                    ui.syn()
+            elif event.code == 1:
+                if event.value < 128:
+                    print('left')
+                    ui.write(e.EV_KEY, e.KEY_UP, 1)
+                    ui.syn()
+                elif event.value > 128:
+                    print('right')
+                    ui.write(e.EV_KEY, e.KEY_DOWN, 1)
+                    ui.syn()
+                else:
+                    print('release')
+                    ui.write(e.EV_KEY, e.KEY_UP, 0)
+                    ui.write(e.EV_KEY, e.KEY_DOWN, 0)
+                    ui.syn()
+            elif event.code == 16:
                 if event.value == -1:
                     print('left')
                     ui.write(e.EV_KEY, e.KEY_LEFT, 1)
