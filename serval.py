465a18fdccb8bc48b025c1350dddb78f2447a5dd
Jakub Podeszwik <jakubpodeszwik@gmail.com>
2016-10-13 20:46:46 +0200

    Second analog stick works now as a mouse - still needs some improvements

--- serval.py
+++ serval.py
@@ -1,4 +1,4 @@
-from evdev import UInput
+from evdev import UInput, AbsInfo
 from evdev import ecodes as e
 import evdev
 
@@ -68,6 +68,35 @@ class AnalogStickProcessor:
         else:
             self.ui.write(e.EV_KEY, e.KEY_UP, 0)
             self.ui.write(e.EV_KEY, e.KEY_DOWN, 0)
+
+        self.ui.syn()
+
+
+class AnalogStick2Processor:
+    def __init__(self, ui):
+        self.ui = ui
+        self.d = 3
+
+    def process_event(self, event):
+        if event.type == 3:
+            if event.code == 2:
+                self.left_right_event(event)
+            elif event.code == 5:
+                self.up_down_event(event)
+
+    def left_right_event(self, event):
+        if event.value < 128:
+            self.ui.write(e.EV_REL, e.REL_X, -self.d)
+        elif event.value > 128:
+            self.ui.write(e.EV_REL, e.REL_X, self.d)
+
+        self.ui.syn()
+
+    def up_down_event(self, event):
+        if event.value < 128:
+            self.ui.write(e.EV_REL, e.REL_Y, -self.d)
+        elif event.value > 128:
+            self.ui.write(e.EV_REL, e.REL_Y, self.d)
 
         self.ui.syn()
 
@@ -154,13 +183,19 @@ def find_razer_serval():
 
 
 def main():
-    ui = UInput()
+    cap = {
+            e.EV_KEY : [e.KEY_A, e.KEY_B, e.KEY_X, e.KEY_Y, e.KEY_L, e.KEY_R, e.KEY_K, e.KEY_E, e.KEY_UP, e.KEY_DOWN, e.KEY_LEFT, e.KEY_RIGHT],
+            e.EV_REL : [
+                (e.REL_Y, AbsInfo(0, 0, 255, 0, 0, 0)),
+                (e.REL_X, AbsInfo(0, 0, 255, 0, 0, 0))
+                ]
+        }
+    ui = UInput(cap, name='razer-serval-virtual-device')
     dev = find_razer_serval()
-    print(dev)
 
     event_processor_classes = [ArrowEventProcessor, AnalogStickProcessor, AButtonProcessor, BButtonProcessor,
                                XButtonProcessor, YButtonProcessor, LBButtonProcessor, RBButtonProcessor,
-                               LTButtonProcessor, RTButtonProcessor]
+                               LTButtonProcessor, RTButtonProcessor, AnalogStick2Processor]
     event_processors = []
 
     for event_processor_class in event_processor_classes:
